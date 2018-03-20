d53455a2448eff3782b9e3e6e3dd4359233b0b9d
CarlMansfield <carlsmanu2005@hotmail.co.uk>
2018-03-20 12:54:05 +0000

    Add project files.

---
+++ ShortenURL/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //All possible base characters
+            const string characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            //Base62 ASCII
+            const int count = 62;
+            //Get input from user
+            long input = long.Parse((Console.ReadLine()));
+            //Create place to store output
+            StringBuilder output = new StringBuilder("");
+
+            do
+            {
+                output.Insert(0, characters[(int)(input % count)]);
+                input = input / count;
+            }
+            while (input > 0);
+
+            Console.WriteLine(output.ToString());
+            Console.ReadLine();
+        }
+    }
+}
